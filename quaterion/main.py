ef1a91429e270689a1f46560a039af369d7fe2e8
generall <andrey@vasnetsov.com>
2021-12-14 16:07:22 +0100

    apply black

--- quaterion/main.py
+++ quaterion/main.py
@@ -2,51 +2,59 @@ from functools import partial
 from typing import Optional, Callable, List, Any
 
 import pytorch_lightning as pl
-from pytorch_lightning.core.memory import ModelSummary
 from torch.utils.data import DataLoader
 
-from quaterion.dataset.similarity_data_loader import PairsSimilarityDataLoader, GroupSimilarityDataLoader
+from quaterion.dataset.similarity_data_loader import (
+    PairsSimilarityDataLoader,
+    GroupSimilarityDataLoader,
+)
 from quaterion.loss.group_loss import GroupLoss
 from quaterion.loss.pairwise_loss import PairwiseLoss
 from quaterion.train.trainable_model import TrainableModel
 
 
 class Quaterion:
-
     @classmethod
-    def combiner_collate_fn(cls, batch: List[Any], features_collate: Callable, labels_collate: Callable):
+    def combiner_collate_fn(
+        cls, batch: List[Any], features_collate: Callable, labels_collate: Callable
+    ):
         features, labels = labels_collate(batch)
         return features_collate(features), labels
 
     @classmethod
-    def fit(cls,
-            trainable_model: TrainableModel,
-            trainer: pl.Trainer,
-            train_dataloader: DataLoader,
-            val_dataloader: Optional[DataLoader] = None,
-            ):
+    def fit(
+        cls,
+        trainable_model: TrainableModel,
+        trainer: pl.Trainer,
+        train_dataloader: DataLoader,
+        val_dataloader: Optional[DataLoader] = None,
+    ):
 
         if isinstance(train_dataloader, PairsSimilarityDataLoader):
             if isinstance(trainable_model.loss, PairwiseLoss):
                 train_dataloader.collate_fn = partial(
                     cls.combiner_collate_fn,
                     features_collate=trainable_model.model.get_collate_fn(),
-                    labels_collate=train_dataloader.__class__.collate_fn
+                    labels_collate=train_dataloader.__class__.collate_fn,
                 )
             elif isinstance(trainable_model.loss, GroupLoss):
-                raise NotImplementedError("Can't use GroupLoss with PairsSimilarityDataLoader")
+                raise NotImplementedError(
+                    "Can't use GroupLoss with PairsSimilarityDataLoader"
+                )
 
         if isinstance(train_dataloader, GroupSimilarityDataLoader):
             if isinstance(trainable_model.loss, GroupLoss):
                 train_dataloader.collate_fn = partial(
                     cls.combiner_collate_fn,
                     features_collate=trainable_model.model.get_collate_fn(),
-                    labels_collate=train_dataloader.__class__.collate_fn
+                    labels_collate=train_dataloader.__class__.collate_fn,
                 )
 
                 train_dataloader.collate_fn = trainable_model.model.get_collate_fn()
             elif isinstance(trainable_model.loss, PairwiseLoss):
-                raise NotImplementedError("Pair samplers are not implemented yet. Try other loss/data loader")
+                raise NotImplementedError(
+                    "Pair samplers are not implemented yet. Try other loss/data loader"
+                )
 
         if val_dataloader is not None:
             val_dataloader.collate_fn = train_dataloader.collate_fn
@@ -54,5 +62,5 @@ class Quaterion:
         trainer.fit(
             model=trainable_model,
             train_dataloaders=train_dataloader,
-            val_dataloaders=val_dataloader
+            val_dataloaders=val_dataloader,
         )

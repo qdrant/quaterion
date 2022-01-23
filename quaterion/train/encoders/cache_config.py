e86d363ca1ba734def647bd4fb75e3efdf6796b6
George Panchuk <george.panchuk@qdrant.tech>
2022-01-24 00:39:56 +0300

    new: separate cache dataloader #17

--- quaterion/train/encoders/cache_config.py
+++ quaterion/train/encoders/cache_config.py
@@ -13,4 +13,7 @@ class CacheType(str, Enum):
 class CacheConfig:
     cache_type: Optional[CacheType] = None
     mapping: Dict[str, CacheType] = field(default_factory=dict)
-    key_extractors: Dict[str, Callable[[Any], Hashable]] = field(default_factory=dict)
+    key_extractors: Dict[str, Callable[[Any], Hashable]] = field(
+        default_factory=dict
+    )
+    batch_size: int = 1

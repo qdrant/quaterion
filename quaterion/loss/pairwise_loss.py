7f1c1ac27b6425522d0c4f10ead83d47d436a513
George Panchuk <george.panchuk@qdrant.tech>
2022-02-03 19:21:55 +0300

    new: fix comment pairs docstring #25

--- quaterion/loss/pairwise_loss.py
+++ quaterion/loss/pairwise_loss.py
@@ -5,14 +5,21 @@ from quaterion.loss.similarity_loss import SimilarityLoss
 
 class PairwiseLoss(SimilarityLoss):
     def __init__(self, distance_metric_name: str = "cosine_distance"):
-        super(PairwiseLoss, self).__init__(distance_metric_name=distance_metric_name)
+        super(PairwiseLoss, self).__init__(
+            distance_metric_name=distance_metric_name
+        )
 
     def forward(
-        self, embeddings: Tensor, pairs: Tensor, labels: Tensor, subgroups: Tensor
+        self,
+        embeddings: Tensor,
+        pairs: Tensor,
+        labels: Tensor,
+        subgroups: Tensor,
     ) -> Tensor:
         """
         :param embeddings: shape: [batch_size x vector_length]
-        :param pairs: shape: [pairs_count x 2] - contains a list of known similarity pairs in batch
+        :param pairs: shape: [pairs_count, 2] - contains a list of known
+        similarity pairs in batch
         :param labels: shape: [pairs_count] - similarity of the pair
         :param subgroups: shape: [pairs_count x 2] - subgroup ids of objects
         :return:

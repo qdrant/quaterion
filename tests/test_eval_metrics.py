320d125fee8a6652f9488b092489f8a503a4ab53
George Panchuk <george.panchuk@qdrant.tech>
2022-02-08 13:52:12 +0300

    tests: add test for eval metrics #25

---
+++ tests/test_eval_metrics.py
@@ -0,0 +1,38 @@
+import pytest
+
+import torch
+
+from quaterion.eval.metrics import (
+    retrieval_reciprocal_rank_2d,
+    retrieval_precision_2d,
+)
+
+
+@pytest.mark.parametrize(
+    "rrk_preds, rrk_targets, expected",
+    [
+        (
+            torch.Tensor([[0.2, 0.5, 0.3], [0.1, 0.1, 0.8]]),
+            torch.Tensor([[0, 0, 1], [0, 0, 1]]),
+            torch.Tensor([0.5, 1]),
+        ),
+    ],
+)
+def test_retrieval_reciprocal_rank_2d(rrk_preds, rrk_targets, expected):
+    assert torch.equal(
+        retrieval_reciprocal_rank_2d(rrk_preds, rrk_targets), expected
+    )
+
+
+@pytest.mark.parametrize(
+    "rp_preds, rp_targets, expected",
+    [
+        (
+            torch.Tensor([[0.2, 0.5, 0.3], [0.1, 0.1, 0.8]]),
+            torch.Tensor([[0, 0, 1], [0, 0, 1]]),
+            torch.Tensor([0.0, 1.0]),
+        ),
+    ],
+)
+def test_retrieval_reciprocal_precision_2d(rp_preds, rp_targets, expected):
+    retrieval_precision_2d(rp_preds, rp_targets)

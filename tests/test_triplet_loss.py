e0b98c9f29a4b9df6fdc82f84921fffb56284286
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-02-18 12:43:32 +0300

    Tests: Add test for Triplet Loss

---
+++ tests/test_triplet_loss.py
@@ -0,0 +1,32 @@
+import torch
+
+from quaterion.loss import TripletLoss
+
+
+class TestTripletLoss:
+    embeddings = torch.Tensor(
+        [
+            [0.0, -1.0, 0.5],
+            [0.1, 2.0, 0.5],
+            [0.0, 0.3, 0.2],
+            [1.0, 0.0, 0.9],
+            [1.2, -1.2, 0.01],
+            [-0.7, 0.0, 1.5],
+        ]
+    )
+
+    groups = torch.LongTensor([1, 2, 3, 3, 2, 1])
+
+    def test_batch_all(self):
+        loss = TripletLoss(mining="all")
+
+        loss_res = loss.forward(embeddings=self.embeddings, groups=self.groups)
+
+        assert loss_res.shape == torch.Size([])
+
+    def test_batch_hard(self):
+        loss = TripletLoss(mining="hard")
+
+        loss_res = loss.forward(embeddings=self.embeddings, groups=self.groups)
+
+        assert loss_res.shape == torch.Size([])

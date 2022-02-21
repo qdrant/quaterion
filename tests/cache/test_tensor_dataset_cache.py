b0bbd5cf4bfacabdc4766a3b039476efd192b820
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-02-21 14:54:51 +0100

    local import of torchvision

--- tests/cache/test_tensor_dataset_cache.py
+++ tests/cache/test_tensor_dataset_cache.py
@@ -3,11 +3,8 @@ import os.path
 import pytest
 
 import pytorch_lightning as pl
-import torchvision
 from quaterion_models.encoders import Encoder
 from torch import nn
-from torchvision import transforms
-from torchvision.datasets import MNIST
 
 from quaterion import Quaterion
 from quaterion.dataset import GroupSimilarityDataLoader
@@ -24,6 +21,7 @@ class MobilenetV3Encoder(Encoder):
 
     def __init__(self, embedding_size=128):
         super().__init__()
+        import torchvision
         self.encoder = torchvision.models.mobilenet_v3_small(pretrained=True)
         self.encoder.classifier = nn.Sequential(nn.Linear(576, embedding_size))
 
@@ -41,6 +39,9 @@ class MobilenetV3Encoder(Encoder):
 
 @pytest.mark.skip(reason="Not yet finished")
 def test_tensor_dataset_cache():
+    from torchvision import transforms
+    from torchvision.datasets import MNIST
+
     tmp_dir_name = os.path.join(os.path.dirname(__file__), "data", "mnist")
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]

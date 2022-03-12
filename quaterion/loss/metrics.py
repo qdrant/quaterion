50b7951b19535f3cee0bb2562f812886b76486ca
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-03-12 11:16:28 +0300

    Implement manhattan distance matrix

--- quaterion/loss/metrics.py
+++ quaterion/loss/metrics.py
@@ -58,19 +58,39 @@ class SiameseDistanceMetric:
         return distances
 
     @staticmethod
-    def manhattan(x: Tensor, y: Tensor, matrix=False) -> Tensor:
+    def manhattan(x: Tensor, y: Tensor = None, matrix: bool = False) -> Tensor:
         """Compute manhattan distance
 
         Args:
             x: shape: (batch_size, ...)
-            y: shape: (batch_size, ...)
-            matrix: flat to calculate distance matrix (all to all)
+            y: shape: (batch_size, ...), optional
+            matrix: if `True` calculate a distance matrix between `x` and `y` (all-to-all).
+                if `y` it is `None`, it assigns `x` to `y`.
+
         Returns:
-            Tensor: shape (batch_size, 1)
+            Tensor: shape (batch_size, batch_size) if `matrix` is `True`, (batch_size, 1) otherwise.
         """
         if not matrix:
-            return torch.pairwise_distance(x, y, p=1)
-        raise NotImplementedError()
+            if y is None:
+                raise ValueError("y cannot be None while matrix is False")
+
+            distances = torch.pairwise_distance(x, y, p=1)
+
+        else:
+
+            if y is None:
+                y = x
+
+            # expand dimensions to calculate element-wise diffrences with broadcasting
+            # shape: batch_size, batch_size, vector_dim)
+            deltas = x.unsqueeze(1) - y.unsqueeze(0)
+            abs_deltas = torch.abs(deltas)
+
+            # sum across the last dimension for eduction
+            # shape: (batch_size, batch_size)
+            distances = abs_deltas.sum(dim=-1)
+
+        return distances
 
     @staticmethod
     def cosine_distance(x: Tensor, y: Tensor = None, matrix=False) -> Tensor:
@@ -119,3 +139,59 @@ class SiameseDistanceMetric:
             if matrix
             else -torch.einsum("id,id->i", x, y)
         )
+
+    @staticmethod
+    def manhattan(
+        x: Tensor, y: Tensor = None, matrix: bool = False, squared: bool = True
+    ) -> Tensor:
+        """Compute manhattan distance
+
+        Args:
+            x: shape: (batch_size, ...)
+            y: shape: (batch_size, ...), optional
+            matrix: if `True` calculate a distance matrix between `x` and `y` (all-to-all).
+                if `y` it is `None`, it assigns `x` to `y`.
+            squared: Squared manhattan distance or not.
+
+        Returns:
+            Tensor: shape (batch_size, batch_size) if `matrix` is `True`, (batch_size, 1) otherwise.
+        """
+        if not matrix:
+            if y is None:
+                raise ValueError("y cannot be None while matrix is False")
+
+            distances = torch.pairwise_distance(x, y, p=1, eps=1e-16)
+        else:
+
+            # Calculate dot product. Shape: (batch_size, batch_size)
+            if y is None:
+                y = x
+
+            dot_product = torch.mm(x, y.transpose(0, 1))
+
+            # get L1 norm by square root of the diagonal. Shape: (batch_size,)
+            square_norm = torch.diagonal(dot_product)
+            mask = (square_norm == 0.0).float()
+            square_norm = square_norm + mask * 1e-16  # numerical stability
+            norm = torch.sqrt(square_norm)
+
+            # calculate distances. Shape: (batch_size, batch_size)
+            mask = (dot_product == 0.0).float()
+            dot_product = dot_product + mask * 1e-16  # numerical stability
+            distances = (
+                norm.unsqueeze(0) - 2.0 * torch.sqrt(dot_product) + norm.unsqueeze(1)
+            )
+            # get rid of negative distances due to calculation errors
+            distances = F.relu(distances)
+
+        if not squared:
+            # handle numerical stability
+            mask = (distances == 0.0).float()
+            distances = distances + mask * 1e-16
+
+            distances = torch.sqrt(distances)
+
+            # Undo trick for numerical stability
+            distances = distances * (1.0 - mask)
+
+        return distances
--- tests/test_distance_metric.py
+++ tests/test_distance_metric.py
@@ -1,9 +1,8 @@
 import torch
-
 from quaterion.loss import SiameseDistanceMetric
 
 
-def test_distance_metric():
+class TestDistanceMetric:
     x = torch.tensor(
         [
             [1.0, 0.0, 1.0],
@@ -14,6 +13,12 @@ def test_distance_metric():
         ]
     )
 
-    res = SiameseDistanceMetric.cosine_distance(x, x, matrix=True)
+    x_dim = x.size()[0]
 
-    print("\n", res)
+    def test_cosine_distance_matrix(self):
+        res = SiameseDistanceMetric.cosine_distance(self.x, matrix=True)
+        assert res.size()[0] == self.x_dim and res.size()[1] == self.x_dim
+
+    def test_manhattan_distance_matrix(self):
+        res = SiameseDistanceMetric.manhattan(self.x, matrix=True)
+        assert res.size()[0] == self.x_dim and res.size()[1] == self.x_dim

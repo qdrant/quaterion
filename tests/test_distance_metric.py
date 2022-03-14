032343e6e07e8e226b303d334d52acdab37106d9
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-03-14 17:31:52 +0300

    Implement manhattan distance matrix (#43)
    * Implement manhattan distance matrix
    
    * docs: auto-generate docs with sphinx
    
    * Cleanup: remove experimental code
    
    * docs: auto-generate docs with sphinx
    
    * docstrings: correct shapes for returned tensors
    
    * docs: auto-generate docs with sphinx
    
    * Code reviews, correct interval for cosine similarity
    
    * fixup: Format Python code with Black
    
    * docs: auto-generate docs with sphinx
    
    * Remove redundant code
    
    * fixup: Format Python code with Black
    
    * No magical numbers
    
    * fixup: Format Python code with Black
    
    Co-authored-by: qdrant <qdrant@users.noreply.github.com>

--- tests/test_distance_metric.py
+++ tests/test_distance_metric.py
@@ -1,19 +1,27 @@
 import torch
-
 from quaterion.loss import SiameseDistanceMetric
 
 
-def test_distance_metric():
+class TestDistanceMetric:
     x = torch.tensor(
         [
-            [1.0, 0.0, 1.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 1.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 1.0, 0.0],
+            [1.0, -1.5, 2.0, -3.0],
+            [-1.0, 1.5, -2.0, 3.0],
         ]
     )
 
-    res = SiameseDistanceMetric.cosine_distance(x, x, matrix=True)
+    x_dim = x.size()[0]
+    expected_cosine_distance = torch.tensor([[0.0, 1.0], [1.0, 0.0]])
+    expected_manhattan_distance = torch.tensor([[0, 15], [15, 0]])
 
-    print("\n", res)
+    def test_cosine_distance_matrix(self):
+        res = SiameseDistanceMetric.cosine_distance(self.x, matrix=True)
+        assert res.size()[0] == self.x_dim and res.size()[1] == self.x_dim
+        assert (res == self.expected_cosine_distance).float().sum() == self.x_dim**2
+
+    def test_manhattan_distance_matrix(self):
+        res = SiameseDistanceMetric.manhattan(self.x, matrix=True)
+        assert res.size()[0] == self.x_dim and res.size()[1] == self.x_dim
+        assert (
+            res == self.expected_manhattan_distance
+        ).float().sum() == self.x_dim**2

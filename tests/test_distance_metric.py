db3d1db215a7f0eca36447d31596843ac95784b7
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-03-14 17:16:44 +0300

    Remove redundant code

--- tests/test_distance_metric.py
+++ tests/test_distance_metric.py
@@ -3,12 +3,7 @@ from quaterion.loss import SiameseDistanceMetric
 
 
 class TestDistanceMetric:
-    x = torch.tensor(
-        [
-            [1.0, -1.5, 2.0, -3.0],
-            [-1.0, 1.5, -2.0, 3.0],
-        ]
-    )
+    x = torch.tensor([[1.0, -1.5, 2.0, -3.0], [-1.0, 1.5, -2.0, 3.0],])
 
     x_dim = x.size()[0]
     expected_cosine_distance = torch.tensor([[0.0, 1.0], [1.0, 0.0]])
@@ -23,6 +18,3 @@ class TestDistanceMetric:
         res = SiameseDistanceMetric.manhattan(self.x, matrix=True)
         assert res.size()[0] == self.x_dim and res.size()[1] == self.x_dim
         assert (res == self.expected_manhattan_distance).float().sum() == 4
-
-
-TestDistanceMetric().test_manhattan_distance_matrix()

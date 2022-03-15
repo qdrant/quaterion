c96f7127ad0472b2d88a69bfab013b3aa1c83a0c
George Panchuk <george.panchuk@qdrant.tech>
2022-03-15 23:01:36 +0300

    refactoring: unwrap short options to make them more descriptive #12

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -6,5 +6,5 @@ set -e
 cd "$(dirname "$0")/../"
 
 
-poetry run sphinx-apidoc -f -e -o docs/source quaterion
+poetry run sphinx-apidoc --force --separate --no-toc -o docs/source quaterion
 poetry run sphinx-build docs/source docs/html

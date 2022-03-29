e1f5b4a5f231e74d46cf332437d1172df1726b6d
George Panchuk <george.panchuk@qdrant.tech>
2022-03-29 18:30:46 +0300

    refactoring: keep test data inside test functions #61

--- tests/metrics/test_group_metric.py
+++ tests/metrics/test_group_metric.py
@@ -1,13 +1,50 @@
-from .params import retrieval_r_precision_params
+import torch
+
 from quaterion.eval.group_metric import RetrievalRPrecision
 
 
-def test_retrieval_r_precision(retrieval_r_precision_params):
-    distance_fn, data, exp_metric = retrieval_r_precision_params
+def dummy_distance_fn(x, y, matrix=True):
+    return x
 
-    metric = RetrievalRPrecision(distance_fn)
 
-    for batch in data:
+def test_retrieval_r_precision():
+    # region dummy tests
+
+    # distances aren't actually calculated, tensor already contains assumed distances
+    # avoids distance calculation and test only metric logic
+
+    # region single batch
+    embeddings = torch.Tensor(
+        [
+            [0.0, 0.3, 0.9, 0.6],
+            [0.3, 0.0, 0.4, 0.15],
+            [0.9, 0.4, 0.0, 0.8],
+            [0.6, 0.15, 0.8, 0.0],
+        ]
+    )
+    groups = torch.LongTensor([1, 2, 1, 2])
+    exp_metric = torch.Tensor([0.5])
+    metric = RetrievalRPrecision(dummy_distance_fn)
+    metric.update(embeddings, groups)
+    assert metric.compute() == exp_metric
+    # endregion single batch
+
+    # region multiple batches
+    first_batch = (
+        torch.Tensor([[0.0, 0.3, 0.9, 0.6], [0.3, 0.0, 0.4, 0.15], ]),  # embeddings
+        torch.LongTensor([1, 2])  # groups
+    )
+    second_batch = (
+        torch.Tensor([[0.9, 0.4, 0.0, 0.8], [0.6, 0.15, 0.8, 0.0]]),  # embeddings
+        torch.LongTensor([1, 2])  # groups
+    )
+
+    exp_metric = torch.Tensor([0.5])
+    metric = RetrievalRPrecision(dummy_distance_fn)
+
+    for batch in (first_batch, second_batch):
         embeddings, groups = batch
         metric.update(embeddings, groups)
     assert metric.compute() == exp_metric
+    # endregion multiple batches
+    # endregion dummy test
--- tests/metrics/test_pair_metric.py
+++ tests/metrics/test_pair_metric.py
@@ -1,22 +1,65 @@
 import pytest
 import torch
-from .params import retrieval_precision_params, retrieval_reciprocal_rank_params
+
 from quaterion.eval.pair_metric import RetrievalPrecision, RetrievalReciprocalRank
 
 
-def test_retrieval_precision(retrieval_precision_params):
-    distance_fn, k, data, exp_metric = retrieval_precision_params
-    metric = RetrievalPrecision(distance_fn, k)
-    for batch in data:
-        embeddings, pairs, labels, subgroups = batch
-        metric.update(embeddings, pairs, labels, subgroups)
-    assert torch.allclose(metric.compute(), exp_metric)
+def dummy_distance_fn(x, y, matrix=True):
+    return x
 
 
-def test_retrieval_reciprocal_rank(retrieval_reciprocal_rank_params):
-    distance_fn, data, exp_metric = retrieval_reciprocal_rank_params
-    metric = RetrievalReciprocalRank(distance_fn)
-    for batch in data:
-        embeddings, pairs, labels, subgroups = batch
-        metric.update(embeddings, pairs, labels, subgroups)
+def test_retrieval_precision():
+    # region dummy tests
+
+    # distances aren't actually calculated, embeddings already contains assumed distances
+    # avoids distance calculation and test only metric logic
+
+    # region k = 1
+    k = 1
+    embeddings = torch.Tensor(
+        [[1, 0.3, 0.2, 0.5], [0, 1, 0.8, 0.6], [0.5, 0.7, 1, 0.8], [1, 0.2, 0.5, 1]]
+    )
+    num_of_pairs = embeddings.shape[0] // 2
+    pairs = torch.LongTensor([[i, i + num_of_pairs] for i in range(num_of_pairs)])
+    labels = torch.Tensor([1.0, 1.0])
+    subgroups = torch.Tensor([1, 2] * 2)
+
+    exp_metric = torch.Tensor([1, 0, 1, 1])
+
+    metric = RetrievalPrecision(dummy_distance_fn, k)
+    metric.update(embeddings, pairs, labels, subgroups)
     assert torch.allclose(metric.compute(), exp_metric)
+    # endregion k = 1
+
+    # region k = 2
+    k = 2
+    exp_metric = torch.Tensor([0.5, 0.5, 0.5, 0.5])
+    metric = RetrievalPrecision(dummy_distance_fn, k)
+
+    metric.update(embeddings, pairs, labels, subgroups)
+    assert torch.allclose(metric.compute(), exp_metric)
+    # endregion k = 2
+    # endregion dummy test
+
+
+def test_retrieval_reciprocal_rank():
+    # region dummy tests
+
+    # distances aren't actually calculated, embeddings already contains assumed distances
+    # avoids distance calculation and test only metric logic
+
+    # region single batch
+    embeddings = torch.Tensor(
+        [[1, 0.3, 0.2, 0.5], [0, 1, 0.8, 0.6], [0.5, 0.7, 1, 0.8], [1, 0.2, 0.5, 1]]
+    )
+    num_of_pairs = embeddings.shape[0] // 2
+    pairs = torch.LongTensor([[i, i + num_of_pairs] for i in range(num_of_pairs)])
+    labels = torch.Tensor([1.0, 1.0])
+    subgroups = torch.Tensor([1, 2] * 2)
+    exp_metric = torch.Tensor([1, 0.5, 1, 1])
+
+    metric = RetrievalReciprocalRank(dummy_distance_fn)
+    metric.update(embeddings, pairs, labels, subgroups)
+    assert torch.allclose(metric.compute(), exp_metric)
+    # endregion single batch
+    # endregion dummy test

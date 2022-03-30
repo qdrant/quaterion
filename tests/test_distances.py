76a863edcaa789daf6d660e6298deae8adc5fd2c
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-03-30 13:28:28 +0300

    Add more tests

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -13,13 +13,36 @@ class TestDistances:
 
     x_dim = x.size()[0]
     expected = {
-        "cosine": {"distance_matrix": torch.tensor([[0.0, 1.0], [1.0, 0.0]])},
-        "manhattan": {"distance_matrix": torch.tensor([[0, 15], [15, 0]])},
+        "cosine": {
+            "distance_matrix": torch.tensor([[0.0, 1.0], [1.0, 0.0]]),
+            "similarity_matrix": torch.tensor([[1.0, 0.0], [0.0, 1.0]]),
+        },
+        "manhattan": {
+            "distance_matrix": torch.tensor([[0.0, 15.0], [15.0, 0.0]]),
+            "similarity_matrix": torch.tensor([[1.0, 0.0625], [0.0625, 1.0]]),
+        },
+        "euclidean": {
+            "distance_matrix": torch.tensor([[0.0, 8.0623], [8.0623, 0.0]]),
+            "similarity_matrix": torch.tensor([[1.0, 0.1103477], [0.1103477, 1.0]]),
+        },
+        "dot_product": {
+            "similarity_matrix": torch.tensor([[16.25, -16.25], [-16.25, 16.25]]),
+            "distance_matrix": torch.tensor([[-16.25, 16.25], [16.25, -16.25]]),
+        },
     }
 
     @pytest.mark.parametrize(
         ("distance_name", "method_name"),
-        [("cosine", "distance_matrix"), ("manhattan", "distance_matrix")],
+        [
+            ("cosine", "distance_matrix"),
+            ("cosine", "similarity_matrix"),
+            ("manhattan", "distance_matrix"),
+            ("manhattan", "similarity_matrix"),
+            ("euclidean", "distance_matrix"),
+            ("euclidean", "similarity_matrix"),
+            ("dot_product", "distance_matrix"),
+            ("dot_product", "similarity_matrix"),
+        ],
     )
     def test_distances(self, distance_name, method_name):
         dist_obj = Distance.get_by_name(distance_name)
@@ -30,5 +53,7 @@ class TestDistances:
                 res = dist_obj.similarity_matrix(self.x)
 
             assert (
-                res == self.expected[distance_name][method_name]
-            ).float().sum() == self.x_dim**2
+                torch.isclose(
+                    res, self.expected[distance_name][method_name], atol=3e-08
+                )  # workaround to avoid small numerical errors
+            ).float().sum() == self.x_dim ** 2

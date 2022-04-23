Several fixes: proper triplet loss scaling, frozen cars encoder

examples/cars/encoders.py
@@ -23,13 +23,14 @@ class CarsEncoder(Encoder):
         else:
             # replace the classifier layer with a newly initialized linear projection
             self._encoder = torchvision.models.mobilenet_v3_small(pretrained=True)
-            self._encoder.classifier = nn.Linear(576, embedding_size)
+            # replace classifier layer with an identity function
+            self._encoder.classifier = nn.Identity()
 
-        self._embedding_size = self._encoder.classifier.out_features
+        self._embedding_size = 576  # last dimension from the base model
 
     @property
     def trainable(self) -> bool:
-        return True
+        return False
 
     @property
     def embedding_size(self) -> int:

examples/cars/models.py
@@ -23,7 +23,7 @@ class Model(TrainableModel):
         return WideningHead(input_embedding_size)
 
     def configure_loss(self) -> SimilarityLoss:
-        return TripletLoss(mining=self._mining)
+        return TripletLoss(mining=self._mining, margin=0.5)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), self._lr)
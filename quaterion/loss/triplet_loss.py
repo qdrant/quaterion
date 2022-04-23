Several fixes: proper triplet loss scaling, frozen cars encoder

quaterion/loss/triplet_loss.py
@@ -115,7 +115,9 @@ class TripletLoss(GroupLoss):
                 # Division by the minimal distance between negative samples scales target distances
                 # # and prevents vector collapse
                 (hardest_positive_dists - hardest_negative_dists)
-                / torch.max(hardest_negative_dists.min(), torch.tensor(1e-16))
+                / torch.max(
+                    hardest_negative_dists.min(), torch.tensor(0.01)
+                )  # this is not an epsilon value, but a reasonable minimum to scale the loss
                 + self._margin
             )
 
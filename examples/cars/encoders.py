02421ac8df6868ac176c902ed4ac1fcf5899930e
Andrey Vasnetsov <andrey@qdrant.tech>
2022-04-25 13:05:26 +0200

    code style suggestions (#94)

--- examples/cars/encoders.py
+++ examples/cars/encoders.py
@@ -4,29 +4,12 @@ import torch
 import torch.nn as nn
 from quaterion_models.encoders import Encoder
 
-try:
-    import torchvision
-except ImportError:
-    import sys
-
-    print("You need to install torchvision for this example:")
-    print("pip install torchvision")
-
-    sys.exit(1)
-
 
 class CarsEncoder(Encoder):
-    def __init__(self, embedding_size: int = 512, restore_path: str = None):
+    def __init__(self, encoder_model: nn.Module):
         super().__init__()
-        if restore_path is not None:
-            self._encoder = torch.load(restore_path)
-        else:
-            # replace the classifier layer with a newly initialized linear projection
-            self._encoder = torchvision.models.resnet18(pretrained=True)
-            # replace classifier layer with an identity function
-            self._encoder.fc = nn.Identity()
-
-        self._embedding_size = 512  # last dimension from the base model
+        self._encoder = encoder_model
+        self._embedding_size = 512  # last dimension from the MobileNetV3 model
 
     @property
     def trainable(self) -> bool:
@@ -45,4 +28,5 @@ class CarsEncoder(Encoder):
 
     @classmethod
     def load(cls, input_path):
-        return CarsEncoder(restore_path=os.path.join(input_path, "encoder.pth"))
+        encoder_model = torch.load(os.path.join(input_path, "encoder.pth"))
+        return CarsEncoder(encoder_model)

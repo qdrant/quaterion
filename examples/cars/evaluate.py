de024d9e7845d74ece3c449e08c3f30b056ae42d
Andrey Vasnetsov <andrey@qdrant.tech>
2022-05-16 15:52:34 +0200

    Cars example eval test (#103)
    * attempt to use evaluator
    
    * fixup: Format Python code with Black
    
    * eval with quaterion
    
    * optional progress logging
    
    * fix imports
    
    Co-authored-by: autoblack <qdrant@users.noreply.github.com>

--- examples/cars/evaluate.py
+++ examples/cars/evaluate.py
@@ -1,81 +1,63 @@
 import argparse
-import numpy as np
 import os
-
 import torch
 import torch.nn as nn
 import torchvision
-import tqdm
+from quaterion_models import MetricModel
+from quaterion_models.heads import EmptyHead
 
 from examples.cars.config import IMAGE_SIZE
+from examples.cars.encoders import CarsEncoder
+from quaterion import Quaterion
+from quaterion.eval.evaluator import Evaluator
 from quaterion.eval.group import RetrievalRPrecision
-from quaterion_models import MetricModel
-
-from .data import get_dataloaders
+from quaterion.eval.samplers.group_sampler import GroupSampler
+from .data import get_datasets
 
 BATCH_SIZE = 32
 METRIC_BATCH = 1024
 
 
-def eval_base_encoder(test_dl, device):
+def eval_base_encoder(dataset, device):
     print("Evaluating base encoder...")
     base_encoder = torchvision.models.resnet152(pretrained=True)
     base_encoder.fc = nn.Identity()
-    base_encoder = base_encoder.to(device)
-    base_encoder.eval()
 
-    all_metrics = []
-    num_metric_batches = 0
-    metric = RetrievalRPrecision()
+    cars_encoder = CarsEncoder(base_encoder)
+    cars_encoder.to(device=device)
+    cars_encoder.eval()
 
-    for i, (_, images, labels) in enumerate(tqdm.tqdm(test_dl)):
-        with torch.no_grad():
-            images = torch.stack(images).to(device)
-            embeddings = base_encoder(images)
-            metric.update(embeddings, labels["groups"].to(device))
+    result = Quaterion.evaluate(
+        evaluator=Evaluator(
+            metrics=RetrievalRPrecision(),
+            sampler=GroupSampler(sample_size=1000, device=device, log_progress=True),
+        ),
+        model=MetricModel(
+            encoders=cars_encoder, head=EmptyHead(cars_encoder.embedding_size)
+        ),
+        dataset=dataset,
+    )
 
-            num_metric_batches += 1
-
-            if METRIC_BATCH / BATCH_SIZE <= num_metric_batches:
-                running_score = float(metric.evaluate())
-                all_metrics.append(running_score)
-                print(f"Running score at step {i}: {running_score:.4f}")
-                metric.reset()
-                num_metric_batches = 0
-
-    final_score = np.mean(all_metrics)
-    print(f"Final Retrieval R-Precision score for base encoder: {final_score:.4f}")
+    print(result)
 
 
-def eval_tuned_encoder(test_dl, device):
+def eval_tuned_encoder(dataset, device):
     print("Evaluating tuned encoder...")
-    tuned_encoder = MetricModel.load(
-        os.path.join(os.path.dirname(__file__), "cars_encoders")
+    tuned_cars_model = MetricModel.load(
+        os.path.join(os.path.dirname(__file__), "best_cars_encoders")
     ).to(device)
-    tuned_encoder.eval()
+    tuned_cars_model.eval()
 
-    all_metrics = []
-    num_metric_batches = 0
-    metric = RetrievalRPrecision()
+    result = Quaterion.evaluate(
+        evaluator=Evaluator(
+            metrics=RetrievalRPrecision(),
+            sampler=GroupSampler(sample_size=1000, device=device, log_progress=True),
+        ),
+        model=tuned_cars_model,
+        dataset=dataset,
+    )
 
-    for i, (_, images, labels) in enumerate(tqdm.tqdm(test_dl)):
-        with torch.no_grad():
-            # images = torch.stack(images).to(device)
-            embeddings = tuned_encoder.encode(
-                images, batch_size=BATCH_SIZE, to_numpy=False
-            )
-            metric.update(embeddings, labels["groups"].to(device))
-            num_metric_batches += 1
-
-            if METRIC_BATCH / BATCH_SIZE <= num_metric_batches:
-                running_score = float(metric.evaluate())
-                all_metrics.append(running_score)
-                print(f"Running score at step {i}: {running_score:.4f}")
-                metric.reset()
-                num_metric_batches = 0
-
-    final_score = np.mean(all_metrics)
-    print(f"Final Retrieval R-Precision score for tuned encoder: {final_score:.4f}")
+    print(result)
 
 
 if __name__ == "__main__":
@@ -92,14 +74,16 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     print("Preparing test data loader...")
-    _, test_dl = get_dataloaders(
-        batch_size=BATCH_SIZE, shuffle=False, input_size=IMAGE_SIZE
+
+    _, test_dataset = get_datasets(
+        input_size=IMAGE_SIZE,
+        split_cache_path="split_cache.pkl",  # reuse existing database split
     )
 
     if args.model == "base":
-        eval_base_encoder(test_dl, device)
+        eval_base_encoder(test_dataset, device)
     elif args.model == "tuned":
-        eval_tuned_encoder(test_dl, device)
+        eval_tuned_encoder(test_dataset, device)
     else:
-        eval_base_encoder(test_dl, device)
-        eval_tuned_encoder(test_dl, device)
+        eval_base_encoder(test_dataset, device)
+        eval_tuned_encoder(test_dataset, device)

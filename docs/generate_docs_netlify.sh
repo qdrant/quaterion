fe9122c419fb4537091c215fc13cb79a27b79f02
George <george.panchuk@qdrant.tech>
2022-05-31 17:49:20 +0300

    fix: pin protobuf version #115 (#116)
    * fix: pin protobuf version #115
    
    * fix: install pytorch-metric-learning for documentation #115
    
    * fix: install pytorch-metric-learning with pip #115

--- docs/generate_docs_netlify.sh
+++ docs/generate_docs_netlify.sh
@@ -11,6 +11,7 @@ pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
 pip install poetry
 poetry build -f wheel
 pip install dist/$(ls -1 dist | grep .whl)
+pip install pytorch-metric-learning==1.3.2
 
 pip install sphinx>=4.4.0
 pip install "git+https://github.com/qdrant/qdrant_sphinx_theme.git@master#egg=qdrant-sphinx-theme"

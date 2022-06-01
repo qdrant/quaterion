b7197aa9f8d0b999aeae692e64f8dd3b7793df09
autoblack <qdrant@users.noreply.github.com>
2022-06-01 22:44:55 +0000

    fixup: Format Python code with Black

--- tests/warnings/test_pl_warnings.py
+++ tests/warnings/test_pl_warnings.py
@@ -10,13 +10,8 @@ def test_ambiguous_batch_warning(fake_trainable_model, fake_pair_dataset):
     fake_trainable_model.setup_dataloader(dataloader)
     trainer = pl.Trainer(max_epochs=1, log_every_n_steps=1)
     with pytest.warns(UserWarning) as records:
-        trainer.fit(
-            fake_trainable_model,
-            dataloader,
-            val_dataloaders=dataloader
-        )
+        trainer.fit(fake_trainable_model, dataloader, val_dataloaders=dataloader)
 
     for record in list(records):
-        if 'Trying to infer' in str(record.message):
+        if "Trying to infer" in str(record.message):
             assert False
-

9fc1bb10af8ac3459e6c749446fdeeab8e09ce45
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-08-11 09:27:35 +0300

    Add XBM loss computation to `TripletLoss`

--- quaterion/loss/triplet_loss.py
+++ quaterion/loss/triplet_loss.py
@@ -159,4 +159,55 @@ class TripletLoss(GroupLoss):
         Returns:
             Tensor: zero-size tensor, XBM loss value.
         """
-        raise NotImplementedError(f"XBM is not implemented for {self.__class__.name}")
+        batch_size = embeddings.size(0)
+
+        # Compute similarity matrix
+        # shape: (batch_size, memory_buffer_size)
+        sim_mat = torch.matmul(embeddings, memory_embeddings.t())
+
+        # split the positive and negative pairs
+        # shape: (batch_size, batch_size)
+        eyes_ = torch.eye(batch_size, dtype=torch.uint8).to(embeddings.device)
+
+        pos_mask = groups.expand(
+            memory_groups.shape[0], batch_size
+        ).t() == memory_groups.expand(batch_size, memory_groups.shape[0])
+        neg_mask = 1 - pos_mask
+        pos_mask[:, :batch_size] = pos_mask[:, :batch_size] - eyes_
+
+        loss = list()
+        neg_count = list()
+
+        for i in range(batch_size):
+            pos_pair_idx = torch.nonzero(pos_mask[i, :]).view(-1)
+            if pos_pair_idx.shape[0] > 0:
+                pos_pair_ = sim_mat[i, pos_pair_idx]
+                pos_pair_ = torch.sort(pos_pair_)[0]
+
+                neg_pair_idx = torch.nonzero(neg_mask[i, :]).view(-1)
+                neg_pair_ = sim_mat[i, neg_pair_idx]
+                neg_pair_ = torch.sort(neg_pair_)[0]
+
+                select_pos_pair_idx = torch.nonzero(
+                    pos_pair_ < neg_pair_[-1] + self._margin
+                ).view(-1)
+                pos_pair = pos_pair_[select_pos_pair_idx]
+
+                select_neg_pair_idx = torch.nonzero(
+                    neg_pair_ > max(0.6, pos_pair_[-1]) - self._margin
+                ).view(-1)
+                neg_pair = neg_pair_[select_neg_pair_idx]
+
+                pos_loss = torch.sum(1 - pos_pair)
+                if len(neg_pair) >= 1:
+                    neg_loss = torch.sum(neg_pair)
+                    neg_count.append(len(neg_pair))
+                else:
+                    neg_loss = 0
+                loss.append(pos_loss + neg_loss)
+            else:
+                loss.append(0)
+        
+        loss = sum(loss) / batch_size
+
+        return loss
